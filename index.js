a4ce7bef0b6e99bf79938ee07f2c55e3e1e8c4fe
RaulAleChoqueValda <89226787+RaulAleChoqueValda@users.noreply.github.com>
2021-08-23 23:08:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,50 @@ app.get("/", (req, res, next) => {
 
 app.post("/tasks", jsonParser, (req, res, next) => {
     req.body.id = tasks.length + 1;
+    req.body.state = 'pending';
     tasks.push(req.body);
     res.send("OK");
 });
 
 app.get("/tasks", (req, res, next) => {
     res.json(tasks);
+});
+
+app.get('/tasks/:taskId',  (req, res, next) => {    
+    var idd = tasks.findIndex( x => x.id === parseInt(req.params.taskId) );
+    res.send(tasks[idd]);
+});
+
+app.put('/tasks/:taskId', jsonParser, (req, res)=> {
+    const status= req.query.state;
+    
+    if(status!= null){
+        var idd = req.params.taskId;
+        tasks.find( (task) =>{
+            if(task.id == idd){
+                task.state=status;
+                res.send("Estado Actualizado");
+            }
+        });
+    }
+    if(status==null){
+        var idd = req.params.taskId;
+        tasks.find( (task) =>{
+            if(task.id == idd){
+                task.title=req.body.title;
+                task.detail=req.body.detail;
+                res.send("Tarea Actualizada");
+            }
+        });
+    }
+    
+});
+
+app.delete('/tasks/:taskId', jsonParser, (req, res, next) => {
+    res.send("Se elimino la tarea con el id: " + req.params.taskId);
+    var idd = tasks.findIndex( x => x.id === parseInt(req.params.taskId) );
+
+    tasks.splice( idd, 1 );
 });
 
 app.listen(3000, () => {
